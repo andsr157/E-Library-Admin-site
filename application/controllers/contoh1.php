2767d3449d64e28ec94f8693f6f203a8c3cdf5c8
andsr157 <andikasatrio159@gmail.com>
2022-09-23 14:05:54 +0700

    add codeigniter3 project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+class contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+        echo"nama saya andika
+        saya tinggal di gabahan
+        olahraga yang saya sukai adalah nothing";
+	}
+}
